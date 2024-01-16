3542f1bc46f975c49232ddaf48195f9a910c55c0
CodeLegg <michaelraymondlegg@gmail.com>
2024-01-16 21:37:57 +0000

    Refactor mobile navbar dropdown and add function for resetting mobile input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,31 @@
-    // JavaScript for toggling the mobile navbar dropdown
-    document.addEventListener("DOMContentLoaded", function () {
-        const mobileToggleBtn = document.getElementById("mobile-toggle-btn");
-        const mobileNavDropdown = document.getElementById("mobile-nav-dropdown");
+// JavaScript for toggling the mobile navbar dropdown
+document.addEventListener("DOMContentLoaded", function () {
+  const mobileToggleBtn = document.getElementById("mobile-toggle-btn");
+  const mobileNavDropdown = document.getElementById("mobile-nav-dropdown");
 
-        mobileToggleBtn.addEventListener("click", function () {
-            mobileNavDropdown.classList.toggle("show");
-            mobileToggleBtn.classList.toggle("cross");
-        });
-    });
+  mobileToggleBtn.addEventListener("click", function () {
+    mobileNavDropdown.classList.toggle("show");
+    mobileToggleBtn.classList.toggle("cross");
+  });
+});
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+// Function for Resetting Mobile Input 
+function closeMobileSearch() {
+  const mobileSearchInput = document.querySelector(".mobile-search-box input");
+
+  // Clear input value
+  if (mobileSearchInput) {
+    mobileSearchInput.value = "";
+  }
+}
+
+// Event listener for closing mobile search
+const mobileCloseBtn = document.getElementById("mobile-close-btn");
+if (mobileCloseBtn) {
+  mobileCloseBtn.addEventListener("click", closeMobileSearch);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+
